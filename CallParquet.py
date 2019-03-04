c76a10e1b2a49e6c0f136c8f31757f76f356f1ae
KathAst <36413340+KathAst@users.noreply.github.com>
2019-03-04 10:13:05 +0100

    added header comment

--- CallParquet.py
+++ CallParquet.py
@@ -1,3 +1,9 @@
+"""
+Runtrough of the DF parquet calculation for the FKM.
+Calls the C++ module SCDMFT.out doing a DMFT calculation first,
+followed by an iterative call of DFParquet.out containing the actual parquet implementation. 
+"""
+
 import subprocess
 import numpy as np
 from shutil import copyfile
